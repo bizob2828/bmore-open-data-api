d7e567163edd7cc65b332d6a3d23c1a70f4fd195
Bob Evans <bizob2828@yahoo.com>
2016-11-12 20:37:43 -0500

    added lat/long data to places

---
+++ utils/get-geo-data.js
@@ -0,0 +1,31 @@
+'use strict';
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+const _ = require('lodash');
+const gMapsClient = require('@google/maps').createClient({
+    key: process.env.GMAPS_KEY
+});
+
+function geocodeData(station) {
+  return new Promise((resolve, reject) => {
+    gMapsClient.geocode({ address: `${station.address} ${station.zip}`}, (err, result) => {
+      if (err) {
+        reject(err);
+      }
+
+      let location = result.json.results[0].geometry.location;
+      station.lat = location.lat;
+      station.long = location.lng;
+      return resolve(station);
+    });
+  });
+}
+
+fs.readFileAsync('./seeders/restaurants.json.orig')
+  .then((data) => {
+    let jsonData = JSON.parse(data.toString());
+    return Promise.map(jsonData, (station) => {
+      return geocodeData(station);
+    });
+  })
+  .then((result) => console.log(JSON.stringify(result)));
